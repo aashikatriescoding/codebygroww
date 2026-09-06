4cc7fa8583251bc43a1b0c473476d77231e69fe5
aashikatriescoding <aashikaa1511@gmail.com>
2026-09-06 19:33:13 +0530

    frontend

--- backend/src/routes/watchlistRoutes.js
+++ backend/src/routes/watchlistRoutes.js
@@ -1,28 +1,4 @@
 
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   addToWatchlist,
-//   getWatchlist,
-//   removeFromWatchlist,
-//   getWatchlistFeed,
-// } = require("../controllers/watchlistController");
-// const { protect } = require("../middleware/authMiddleware");
-
-// router.use(protect);
-
-// router.get("/feed", getWatchlistFeed);
-// router.post("/", addToWatchlist);
-// router.get("/", getWatchlist);
-// router.delete("/:id", removeFromWatchlist);
-
-// module.exports = router;
-
-
-
-
-
-
 
 const express = require("express");
 const router = express.Router();
